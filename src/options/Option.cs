297c09f21b78ce59ab380bc51ad1b7f2ccdbb94d
larrykirschner <larrykirschner@gmail.com>
2017-09-08 15:24:36 -0700

    0.0.1

---
+++ src/options/Option.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeatThat
+{
+	/// <summary>
+	/// Use when you are passing an options IDictionary<string, object> and you want to make some valid options visible in the API.
+	/// Depends on ConvertTypeExt extension method.
+	/// </summary>
+	public class Option<T>
+	{
+		public Option(string k)
+		{
+			this.key = k;
+		}
+
+		/// <summary>
+		/// The dictionary key the option sets
+		/// </summary>
+		/// <value>The key.</value>
+		public string key { get; private set; }
+
+		/// <summary>
+		/// Accepted value type for the option.
+		/// If an option allows multiple types, use object
+		/// </summary>
+		public Type type { get { return typeof(T); } }
+
+
+		/// <summary>
+		/// Set a value for the option onto an <c>IDictionary</c>.
+		/// </summary>
+		public void Set(T value, IDictionary<string, object> opts)
+		{
+			opts[this.key] = value;
+		}
+
+		/// <summary>
+		/// Gets the current value for this option from a an <c>IDictionary</c>.
+		/// If the value can't be found (or if it can't be converted to the required type)
+		/// returns the provided default.
+		/// </summary>
+		public T Get(IDictionary<string, object> opts, T defaultVal = default(T)) 
+		{
+			if(opts == null) {
+				return defaultVal;
+			}
+
+			object o;
+			T v;
+			return opts.TryGetValue(this.key, out o)? 
+				(o.TryConvertTo<T>(out v)? v: defaultVal) : defaultVal;
+		}
+	}
+
+	public class Option : Option<object> 
+	{
+		public Option(string k) : base(k) {}
+	}
+}
